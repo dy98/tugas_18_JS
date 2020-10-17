1f2763e7aa68183f69548b7d325cc4372fbb66ba
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 15:43:32 +0700

    tugas 18 JS
    tugas Untuk Niomic

---
+++ tugas18.js
@@ -0,0 +1,6 @@
+function hilangKata() {
+    let kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+
+    console.log(kata.slice(8, 24));
+}
+hilangKata();
